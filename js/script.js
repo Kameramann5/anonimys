b1ebc3aab34521223ca68f9a8568b901af992b43
camera <example@gmail.com>
2020-07-22 16:56:00 +0300

    five

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");  
-
 console.log(numberOfFilms);  
 const personalMovieDB = { 
-
     count: numberOfFilms, 
-
     movies: {}, 
-
     actors: {}, 
-
     genres: [], 
-
     privat: false 
-
 }; 
-const a = prompt("Один из последних просмотренных фильмов?",""), 
-b = prompt("На сколько оцените его?",""), 
-c= prompt("Один из последних просмотренных фильмов?",""), 
-d = prompt("На сколько оцените его?","");
+for (let i=0;i<2;i++)
+{
+    const a = prompt("Один из последних просмотренных фильмов?",""), 
+b = prompt("На сколько оцените его?","");
+if (a!=null && b!=null && a!='' && b!='' && a.length<50 ) { 
 personalMovieDB.movies[a]=b; 
-personalMovieDB.movies[c]=d; 
+console.log('done');
+}
+else {
+    console.log('error');
+i--;
+} }
+
+if (personalMovieDB.count<10) {console.log("просмотрено мало фильмов");}
+else if (personalMovieDB.count>=10 && personalMovieDB.count<30) 
+{console.log("Вы классический зритель");}
+else if (personalMovieDB.count>=30) {console.log("Вы киноман")}
+else {
+    console.log("Произошла ошибка")
+
+}
 console.log(personalMovieDB);
